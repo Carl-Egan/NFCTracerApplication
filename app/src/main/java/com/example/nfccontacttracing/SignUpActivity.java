dc15a810c44a6efa63d51ea4445a00e7ac9ec21d
carl <carlegan1997@gmail.com>
2021-02-05 18:43:35 +0000

    Styling LoginActivity and SignUpActivity + adding app icon

--- app/src/main/java/com/example/nfccontacttracing/SignUpActivity.java
+++ app/src/main/java/com/example/nfccontacttracing/SignUpActivity.java
@@ -3,8 +3,6 @@ package com.example.nfccontacttracing;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -12,41 +10,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 
-   private EditText eEmail;
-   private EditText ePassword;
+   private TextInputEditText eEmail;
+   private TextInputEditText ePassword;
+   private TextInputEditText ePhoneNum;
+   private TextInputEditText eName;
    private Button btnSignUp;
-   TextView tvSignIn;
+   private Button btnSignIn;
    FirebaseAuth mFirebaseAuth;
 
 
-    boolean isValid = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
 
         eEmail = findViewById(R.id.etEmailAddress);
+        eName = findViewById(R.id.etName);
+        ePhoneNum = findViewById(R.id.etPhoneNum);
         ePassword = findViewById(R.id.etPassword);
         btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignIn = findViewById(R.id.btnSignIn);
+
         mFirebaseAuth = FirebaseAuth.getInstance();
-        tvSignIn = findViewById(R.id.textView);
 
         btnSignUp.setOnClickListener(v -> {
 
+            String inputPhone = ePhoneNum.getText().toString();
+            String inputName = eName.getText().toString();
             String inputEmail = eEmail.getText().toString();
             String inputPassword = ePassword.getText().toString();
+
             if (inputEmail.isEmpty()) {
-                eEmail.setError("Please enter email id");
+                eEmail.setError("Please Enter An Email");
                 eEmail.requestFocus();
             }
             else if(inputPassword.isEmpty()){
-                ePassword.setError("Please enter your password");
+                ePassword.setError("Please Enter A Password");
+                ePassword.requestFocus();
+            }
+            else if(inputName.isEmpty()){
+                ePassword.setError("Please Enter Your Name");
+                ePassword.requestFocus();
+            }
+            else if(inputPhone.isEmpty()){
+                ePassword.setError("Please Enter Your Phone Number");
                 ePassword.requestFocus();
             }
             else if(inputEmail.isEmpty() && inputPassword.isEmpty()) {
@@ -71,7 +85,7 @@ public class SignUpActivity extends AppCompatActivity {
 
              }
             });
-        tvSignIn.setOnClickListener(v -> {
+        btnSignIn.setOnClickListener(v -> {
             Intent i = new Intent(SignUpActivity.this, LoginActivity.class);
             startActivity(i);
         });
