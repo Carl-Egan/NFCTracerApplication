be89b8c64cefb759f332f419377eecba09b8363b
carl <carlegan1997@gmail.com>
2021-03-08 16:50:31 +0000

    Styling Home page

--- app/src/main/java/com/example/nfccontacttracing/activities/HomePageActivity.java
+++ app/src/main/java/com/example/nfccontacttracing/activities/HomePageActivity.java
@@ -2,11 +2,14 @@ package com.example.nfccontacttracing.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.nfccontacttracing.R;
+import com.example.nfccontacttracing.fragments.Stats;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -15,15 +18,20 @@ public class HomePageActivity extends AppCompatActivity {
 
     BottomNavigationView navigation;
     FloatingActionButton floatingActionButton;
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-
+        button = findViewById(R.id.stats);
 
         navigation = findViewById(R.id.bottomNavigationView);
         floatingActionButton = findViewById(R.id.fab);
+
+        button.setOnClickListener(v -> {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.home_page, new Stats()).commit();
+        });
 
         navigation.setOnNavigationItemSelectedListener(item -> {
             switch (item.getItemId()) {
